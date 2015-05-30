ea9ab605297de4c709bbcece46f7a4d8c96f62bc
Aaron Kling <webgeek1234@gmail.com>
2015-05-30 12:49:39 -0500

    kexec: Add Kexec HardBoot Support
    Change-Id: I915961ecde831a6940248d715f6f416d80c2c0ad
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    (cherry picked from commit b47ac32d8d28e185003638aae65e151cefc201e1)
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- arch/arm64/include/asm/assembler.h
+++ arch/arm64/include/asm/assembler.h
@@ -177,6 +177,40 @@ lr	.req	x30		// link register
 	orr	\rd, \lbits, \hbits, lsl #32
 	.endm
 
+ /* __ASM_ASSEMBLER_H */
+ /* vma_vm_mm - get mm pointer from vma pointer (vma->vm_mm)
+  */
+	.macro	vma_vm_mm, rd, rn
+	ldr	\rd, [\rn, #VMA_VM_MM]
+	.endm
+
+/*
+ * mmid - get context id from mm pointer (mm->context.id)
+ */
+	.macro	mmid, rd, rn
+	ldr	\rd, [\rn, #MM_CONTEXT_ID]
+	.endm
+
+/*
+ * dcache_line_size - get the minimum D-cache line size from the CTR register.
+ */
+	.macro	dcache_line_size, reg, tmp
+	mrs	\tmp, ctr_el0			// read CTR
+	ubfm	\tmp, \tmp, #16, #19		// cache line size encoding
+	mov	\reg, #4			// bytes per word
+	lsl	\reg, \reg, \tmp		// actual cache line size
+	.endm
+
+/*
+ * icache_line_size - get the minimum I-cache line size from the CTR register.
+ */
+	.macro	icache_line_size, reg, tmp
+	mrs	\tmp, ctr_el0			// read CTR
+	and	\tmp, \tmp, #0xf		// cache line size encoding
+	mov	\reg, #4			// bytes per word
+	lsl	\reg, \reg, \tmp		// actual cache line size
+	.endm
+
 /*
  * Pseudo-ops for PC-relative adr/ldr/str <reg>, <symbol> where
  * <symbol> is within the range +/- 4 GB of the PC.
@@ -341,4 +375,4 @@ lr	.req	x30		// link register
 	movk	\reg, :abs_g0_nc:\val
 	.endm
 
-#endif	/* __ASM_ASSEMBLER_H */
+#endif
