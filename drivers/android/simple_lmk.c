284fed540fe1b23086d918c7242448d4e82ccf19
dopaemon <polarisdp@gmail.com>
2021-08-25 00:10:53 +0700

    Revert "simple_lmk: Mark victim thread group with TIF_MEMDIE"
    This reverts commit 6acc5bc1b9ff27803640eb02befd164a2951e69f.
    
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -218,10 +218,10 @@ static void scan_and_kill(unsigned long pages_needed)
 		do_send_sig_info(SIGKILL, SEND_SIG_FORCED, vtsk, true);
 
 		/* Mark the thread group dead so that other kernel code knows */
-		rcu_read_lock();
+		/*rcu_read_lock();
 		for_each_thread(vtsk, t)
 			set_tsk_thread_flag(t, TIF_MEMDIE);
-		rcu_read_unlock();
+		rcu_read_unlock();*/
 
 		/* Elevate the victim to SCHED_RR with zero RT priority */
 		sched_setscheduler_nocheck(vtsk, SCHED_RR, &sched_zero_prio);
